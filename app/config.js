be13768c6c6d0f0d273a04b61707930b50762c2d
liuxw <986503713@qq.com>
2021-12-14 23:00:57 +0800

    first commit

---
+++ app/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  connectionStr: 'mongodb://liuxw:liuxw123@zhihu-shard-00-00.nspix.mongodb.net:27017,zhihu-shard-00-01.nspix.mongodb.net:27017,zhihu-shard-00-02.nspix.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-2d3lrc-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
